33fdb1706816c4500ddb12f59af998aee7e033ca
TranThinh <Bigfoot999>
2025-03-25 22:42:22 +0700

    Bai 7

---
+++ Module2BaiSo7_TranQuocThinh/frmLight.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Module2BaiSo7_TranQuocThinh
+{
+    partial class frmLight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblNotify = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picTurnOff = new System.Windows.Forms.PictureBox();
+            this.picTurnOn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOff)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNotify
+            // 
+            this.lblNotify.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblNotify.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNotify.Font = new System.Drawing.Font("Times New Roman", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotify.Location = new System.Drawing.Point(35, 362);
+            this.lblNotify.Name = "lblNotify";
+            this.lblNotify.Size = new System.Drawing.Size(272, 43);
+            this.lblNotify.TabIndex = 2;
+            this.lblNotify.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Designed  by: Trần Quốc Thịnh";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(227, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Jack";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picTurnOff
+            // 
+            this.picTurnOff.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picTurnOff.Image = global::Module2BaiSo7_TranQuocThinh.Properties.Resources.lightoff;
+            this.picTurnOff.Location = new System.Drawing.Point(70, 115);
+            this.picTurnOff.Name = "picTurnOff";
+            this.picTurnOff.Size = new System.Drawing.Size(199, 203);
+            this.picTurnOff.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTurnOff.TabIndex = 3;
+            this.picTurnOff.TabStop = false;
+            this.toolTip1.SetToolTip(this.picTurnOff, "Click me to Turn On the Light!");
+            this.picTurnOff.Visible = false;
+            this.picTurnOff.Click += new System.EventHandler(this.picTurnOff_Click);
+            // 
+            // picTurnOn
+            // 
+            this.picTurnOn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picTurnOn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picTurnOn.Image = global::Module2BaiSo7_TranQuocThinh.Properties.Resources.lighton;
+            this.picTurnOn.Location = new System.Drawing.Point(70, 114);
+            this.picTurnOn.Name = "picTurnOn";
+            this.picTurnOn.Size = new System.Drawing.Size(199, 204);
+            this.picTurnOn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTurnOn.TabIndex = 2;
+            this.picTurnOn.TabStop = false;
+            this.toolTip1.SetToolTip(this.picTurnOn, "Click me to Turn OFF the Light!");
+            this.picTurnOn.Click += new System.EventHandler(this.picTurnOn_Click);
+            // 
+            // frmLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 487);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNotify);
+            this.Controls.Add(this.picTurnOff);
+            this.Controls.Add(this.picTurnOn);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLight";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLight";
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOff)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTurnOn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picTurnOn;
+        private System.Windows.Forms.PictureBox picTurnOff;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblNotify;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Module2BaiSo7_TranQuocThinh/frmLight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module2BaiSo7_TranQuocThinh
+{
+    public partial class frmLight : Form
+    {
+        public frmLight()
+        {
+            InitializeComponent();
+            lblNotify.Text = "Jack. Turn OFF the Light, please!";
+        }
+
+        private void picTurnOn_Click(object sender, EventArgs e)
+        {
+            picTurnOff.Visible = true;
+            picTurnOn.Visible = false;
+            lblNotify.Text = "Jack. Turn ON the Light, please!";
+        }
+
+        private void picTurnOff_Click(object sender, EventArgs e)
+        {
+            picTurnOff.Visible = false;
+            picTurnOn.Visible = true;
+            lblNotify.Text = "Jack. Turn OFF the Light, please!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
